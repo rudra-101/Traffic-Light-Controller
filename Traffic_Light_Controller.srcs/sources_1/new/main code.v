c68d8f79df9be0dd7bd65af739ee524c68dc1952
Rudra Vaibhav <rudravaibhav45@gmail.com>
2023-07-04 20:48:25 +0530

    System Files

---
+++ Traffic_Light_Controller.srcs/sources_1/new/main code.v
@@ -0,0 +1,26 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date: 04.07.2023 10:20:50
+// Design Name: 
+// Module Name: main code
+// Project Name: 
+// Target Devices: 
+// Tool Versions: 
+// Description: 
+// 
+// Dependencies: 
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+// 
+//////////////////////////////////////////////////////////////////////////////////
+
+
+module main code(
+
+    );
+endmodule
---
+++ Traffic_Light_Controller.srcs/sources_1/new/main_code.v
@@ -0,0 +1,26 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date: 04.07.2023 10:21:53
+// Design Name: 
+// Module Name: main_code
+// Project Name: 
+// Target Devices: 
+// Tool Versions: 
+// Description: 
+// 
+// Dependencies: 
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+// 
+//////////////////////////////////////////////////////////////////////////////////
+
+
+module main_code(
+
+    );
+endmodule
